108da23e6645109ff28133726c455c536dfcc9df
mihailik <mihailik@gmail.com>
2024-03-05 17:03:53 +0000

    Simplify.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-(() => {
-  // src/index.js
-  console.log("INDEX");
-})();
-//# sourceMappingURL=index.js.map
+// @ts-check
+
+function cunningWORK() {
+  if (typeof window !== "undefined" && window?.navigator?.serviceWorker)
+    return runBrowserWindow();
+  else if (typeof self !== "undefined" && typeof self?.Array === 'function')
+    return runServiceWorker();
+
+  async function runBrowserWindow() {
+    console.log('cunningWORK: from runBrowserWindow');
+    const registration = await navigator.serviceWorker.register(
+      'index.js',
+      {
+        scope: location.pathname,
+      }
+    );
+  }
+
+  function runServiceWorker() {
+    console.log('cunningWORK: from - runServiceWorker');
+    // navigator.serviceWorker.addEventListener('message', e => {
+    //     console.log(e.data);
+    // });
+  }
+
+} cunningWORK();
